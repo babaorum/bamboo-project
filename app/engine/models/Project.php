dc68b474645c5c85724e9cdc6c891256e4ef7225
rgrelet <rgrelet@kilix.fr>
2014-06-25 15:27:10 +0000

    create Model and Controller for Project

---
+++ app/engine/models/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\engine\models;
+
+use R;
+use Walrus\core\WalrusModel;
+
+class Project extends WalrusModel
+{
+	public function new()
+	{
+		
+	}
+	
+	public function create()
+	{
+
+	}
+
+}
