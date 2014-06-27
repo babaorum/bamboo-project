7c15d80fdc7e3d07e0211f635a2880217041e259
Romain Grelet <romain.grelet@supinternet.fr>
2014-06-27 07:15:30 +0000

    fix VHOST.md + config/compiled on sub branch

--- config/compiled/routes/route-default.php
+++
@@ -1,1 +0,0 @@
-a:2:{s:5:"_home";a:4:{s:4:"path";s:0:"";s:10:"controller";s:17:"ProjectController";s:6:"action";s:3:"run";s:6:"method";s:3:"GET";}s:7:"_config";a:3:{s:4:"path";s:6:"config";s:10:"controller";s:16:"ConfigController";s:6:"action";s:6:"config";}}
