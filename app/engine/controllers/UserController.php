183f47227881f11350c443ee21d51bf833efb6af
Romain Grelet <romain.grelet@supinternet.fr>
2014-06-28 00:17:00 +0000

    fix naming for model instance

--- app/engine/controllers/UserController.php
+++ app/engine/controllers/UserController.php
@@ -6,13 +6,13 @@ use Walrus\core\WalrusController;
 
 class UserController extends WalrusController
 {
-	public function home()
-	{
-		$project = $this->model('project')
-		
-		$result = $project->getProjects();
+    public function home()
+    {
+        $projectModel = $this->model('project')
+        
+        $result = $projectModel->getProjects();
 
-		if(empty($result))
+        if(empty($result))
         {
             $this->register('noProject', 'Il n\'y a pas encore de projet');
         }
